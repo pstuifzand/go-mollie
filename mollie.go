f6358027473a713f213c4805e4b39d35f7c26c52
Peter Stuifzand <peter@stuifzand.eu>
2013-01-22 17:22:28 +0100

    Add copyright notice at the top

--- mollie.go
+++ mollie.go
@@ -1,4 +1,7 @@
 /*
+mollie.go - connect to the Mollie iDEAL API
+Copyright (c) 2013 Peter Stuifzand
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
